f5f9f46ac8d71cfb956edf721a3aee8371debfa4
Felipe <f.sanmiguel@uniandes.edu.co>
2026-04-19 15:33:54 -0500

    Creacion de clases e inclusion de archivo de parametros configurables - config.txt

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HashMap<String, Integer> config = new HashMap<>();
+
+        Scanner sc = new Scanner(new File("config.txt"));
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] partes = line.split("=");
+            config.put(partes[0], Integer.parseInt(partes[1]));
+        }
+        sc.close(); 
+
+        int ni = config.get("ni");
+        int nc = config.get("nc");
+
+        System.out.println(ni);
     }
 }
